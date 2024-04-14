c3f21d31b1416a7cd7435285078144792cdd83d2
Pallavi Panwar <terminal.pallavi@gmail.com>
2024-04-15 00:51:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Windows 11 Clone
+Windows 11 clone using `HTML`, `CSS`, and `Javascript`. Feel free to use any of the code anywhere you want.
+
+This was originally motivated by one of our favorite YouTubers, `CodeWithHarry`. He made a video on this clone but he used screenshots. At the end of the video, he asked to take it to next level and do it using only `CSS`. So, that's what I did.
+
+Although I am not on the path to be Web Developer, but I love creating beatuful UIs.
+
+You are free to use the code in this project anywhere you like. I do own it but it's code and we are programmers. Code doesn't really belong to just one of us :D. Seriously though, use the code as you wish. Just give some sort of credit in the corner. I'll do appreciate that. xD
+
+# Preview
+All of the assets are in `svg` format. The searching also works. Each button is also clickable.
+
---
+++ main.js
@@ -0,0 +1,127 @@
+let startMenu = document.getElementById('start');
+let body = document.getElementById('body');
+
+let myShit = `<div class="myNewShit" style="position: absolute;
+                    z-index: 417047847385;
+                    left: 0%;
+                    top: 0%;
+                    background-color: var(--hovColor);
+                    height: 100vh;
+                    width: 100%;
+                    backdrop-filter: blur(5px);
+                    ">
+                    <div class="shitMsg" style="position: absolute;
+                    left: 50%;
+                    top: 50%;
+                    transform: translate(-50%, -50%);
+                    height: 10%;
+                    width: 25%;
+                    background-color: rgba(38, 38, 38, 0.866);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 12px;
+                    color: var(--textColor);">
+                        <p>NULL</p>
+                    </div>
+                    </div>
+                            `;
+
+let myNewShit = document.getElementById('myNewShit');
+
+startMenu.addEventListener('click', function () {
+    document.querySelector('.popup').classList.toggle("popActive");
+    document.querySelector('.clicked').classList.toggle("hovActive");
+})
+
+let search = document.getElementById('search-box');
+search.addEventListener('input', function () {
+    let searched = search.value.toLowerCase();
+    let apps = document.getElementsByClassName('appImg');
+    Array.from(apps).forEach(function (button) {
+        let appName = button.getElementsByClassName('recAppName')[0].innerText.toLowerCase();
+        if (appName.startsWith(searched) || appName.includes(searched)) {
+            button.style.display = "flex";
+        }
+        else {
+            button.style.display = "none";
+        }
+    })
+})
+
+let notApps = document.querySelectorAll('.easyDoesIt');
+notApps.forEach(function(el){
+    el.addEventListener('click', function () {
+        
+        myNewShit.innerHTML = myShit;
+    
+        body.appendChild(myNewShit);
+    
+        setTimeout(() => {
+            myNewShit.innerHTML = "";
+        }, 2000);
+    })
+})
+
+let powerBtn = document.getElementById('p-btn');
+powerBtn.addEventListener('click', function(){
+    document.querySelector('.power-menu').classList.toggle("power-menu-vis");
+})
+
+let nada = document.querySelectorAll('.nada');
+nada.forEach(function(el){
+    el.addEventListener('click', function () {
+        
+        myNewShit.innerHTML = `<div class="myNewShit" style="position: absolute;
+        z-index: 417047847385;
+        left: 0%;
+        top: 0%;
+        background-color: var(--hovColor);
+        height: 100vh;
+        width: 100%;
+        backdrop-filter: blur(5px);
+        ">
+        <div class="shitMsg" style="position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        height: 10%;
+        width: 25%;
+        background-color: rgba(38, 38, 38, 0.866);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 12px;
+        color: var(--textColor);">
+            <p>Ab yeh to kisi kitab me nhi likha!</p>
+        </div>
+        </div>
+                `;
+    
+        body.appendChild(myNewShit);
+    
+        setTimeout(() => {
+            myNewShit.innerHTML = "";
+        }, 1500);
+    })
+})
+const setDate = function () {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    const taskDate = document.querySelector(`#taskDate`);
+    taskDate.innerHTML = `${day}/${month + 1}/${year}`;
+}
+
+const setTime = function(){
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const seconds = date.getSeconds();
+    const taskTime = document.querySelector(`#taskTime`);
+    taskTime.innerHTML = `${hour} : ${minute} : ${seconds}`;
+}
+const refresh = 1000;  //this will fetch the time every one second
+setInterval(setDate, refresh);
+setInterval(setTime, refresh);
